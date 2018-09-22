9b6abea0b91e1dccb25c448432b2fb619f959697
Maykal Aliev <maykalz2001@gmail.com>
2018-09-22 14:18:12 +0300

    Без карты
    Signed-off-by: Maykal Aliev <maykalz2001@gmail.com>

---
+++ js/icon_customImage.js
@@ -0,0 +1,35 @@
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [55.751574, 37.573856],
+            zoom: 9
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: ' https://svsleep.ru/catenergy/img/map/map-pin.png',
+            // Размеры метки.
+            iconImageSize: [124, 106],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        })
+
+        
+
+    myMap.geoObjects
+        .add(myPlacemark)
+        .add(myPlacemarkWithContent);
+});
